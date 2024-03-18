38a48b65657c08684cbc7eb3026009051d61dc45
mamedlin <148177859+mamedlin@users.noreply.github.com>
2024-03-17 22:37:07 -0500

    Adding completed Swift UI project files

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojiArr = ["🤣", "😜", "😀", "🤩", "😂"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(emojiArr, id: \.self) { emoji in
+                    ListItem(emoji: emoji)
+                    
+                }
+            }
+            .navigationTitle("Emoji Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
+struct ListItem: View {
+    let emoji : String
+    @State var count : Int = 0
+    
+    var body: some View
+    {
+        HStack {
+            Text(emoji)
+            Text("Counter: \(count)")
+                .foregroundColor(.red)
+            Spacer()
+            Button(action: {count += 1}) {
+                Image(systemName: "plus.rectangle")
+                
+                    .font(.title)
+                    .foregroundColor(.red)
+            }
+            Button(action: {count -= 1}) {
+                Image(systemName: "minus.rectangle")
+                
+                    .foregroundColor(.red)
+                    .font(.title)
+            }
+
+        }
+        .buttonStyle(.borderless)
+        .padding()
+    }
+}
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
